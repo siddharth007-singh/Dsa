816a7047339762cb73e3d81a9fce6af5bd8d51fd
siddharth007-singh <siddharth.code.cws@gmail.com>
2022-12-24 13:31:15 +0530

    Stack Testing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
+#include <stack>
 using namespace  std;
 
 int main() {
-    cout << "Hello, Stack!" <<endl;
+    //creation of stack
+    stack <int> s;
+
+    //Push Operations
+    s.push(2);
+    s.push(3);
+
+    //pop Opertaion
+    s.pop();
+
+    cout<<"Printing the top Element "<<s.top()<<" "<<endl;
+    if(s.empty()){
+        cout<<"Stack is Empty";
+    }
+    else{
+        cout<<"Stack is not Empty";
+    }
     return 0;
 }
