07f6e4d9ba9efa9f42511a60397599b99de4eb41
siddharth007-singh <siddharth.code.cws@gmail.com>
2022-12-29 20:24:07 +0530

    Reverse_staring

--- Stack/Reverse.cpp
+++ Stack/Reverse.cpp
@@ -1,8 +1,29 @@
 #include <iostream>
+#include<stack>
 using namespace std;
 
-
 int main() {
-    cout << "Hello, World!" <<endl;
+    string str = "Siditi";
+
+    stack<char>s;
+
+    //Filling the data in stack
+    for(int i=0; i<str.length(); i++){
+        char ch = str[i];
+        s.push(ch);
+    }
+
+    //Store the answer after pop
+    string ans = "";
+
+    while(!s.empty()){
+        char ch = s.top(); //Picking the char which is place on the top
+        ans.push_back(ch); //puting the ch in empty ans stack
+        s.pop();
+    }
+
+    cout<<"The Reverse of String is : "<<ans<<endl;
+
+
     return 0;
 }
