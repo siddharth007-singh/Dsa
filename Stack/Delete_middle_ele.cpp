2318a7286477d5e8e7fb6cee6e7c0eca3753eee2
siddharth007-singh <siddharth.code.cws@gmail.com>
2022-12-30 12:52:46 +0530

    Delete_middle

--- Stack/Delete_middle_ele.cpp
+++ Stack/Delete_middle_ele.cpp
@@ -2,13 +2,26 @@
 #include<stack>
 using namespace std;
 
-void solve(stack<int>s, int size){
-    //base_case
-    int count = 0;
-    if(count==size/2){
+void solve(stack<int>s, int count, int n){
+    //base
+    if(count==n/2){
         s.pop();
         return;
     }
+
+    //Temprory frist element out 
+    int num = s.top();
+    s.pop();
+
+    //Recursive Call
+    solve(s, count+1, n);
+    //now comming back
+    s.push(num);
+}
+
+void deleteMiddle(stack<int>inputStack, int size){
+    int count = 0;
+    solve(inputStack, count, N);
 }
 
 int main() {
@@ -16,6 +29,6 @@ int main() {
     s.push(22);
     s.push(55);
     s.push(32);
-    solve(s, count, size);
-    cout<<s.top()<<endl;
+    // solve(s, count, size);
+    cout<<"The Top Element is : "<<s.top()<<endl;
 }
