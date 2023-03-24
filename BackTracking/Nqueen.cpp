a31e95e3c2964fee9e2b4dbfb33b2925ad5c58bd
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-03-24 22:06:07 +0530

    24March2023

---
+++ BackTracking/Nqueen.cpp
@@ -0,0 +1,89 @@
+// 0 = empty cell
+// 1 = queen is placed 
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void printSolve(vector<vector<int>> &board, int n){
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout<<board[i][j]<<endl;
+        }
+    }
+
+    cout<<endl;
+}
+
+bool isSafe(int row, int col, vector<vector<int>> &board, int n){
+    //checking wheater we put queen in curr cell or not
+
+    //Checking row
+    int i=row;
+    int j=col;
+
+    while(j>=0){
+        if(board[i][j]==1){
+            return false;
+        }
+        j--;
+    }
+
+    //check upper left and bottom left (Diagonal area)
+
+    //Upper Left
+    i=row, j=col;
+    while(i>=0 && j>=0){
+        if(board[i][j]==1){
+            return false;
+        }
+        i--;
+        j--;
+    }
+
+    //bottom left
+    i=row, j=col;
+    while(i<n && j>=0){
+        if(board[i][j]==1){
+            return false;
+        }
+        i++;
+        j--;
+    }
+
+
+    //queen not found Hence position is safe
+    return true;
+}
+
+
+void solve(vector<vector<int>> &board, int col, int n){
+    //base case
+    if(col>=n){
+        printSolve(board, n);
+        return;
+    }
+
+    for(int row = 0; row<n; row++){
+        if(isSafe(row, col, board, n)){
+            //put the queen
+            board[row][col] = 1;
+
+            //recursive call
+            solve(board, col+1, n);
+
+            //backtracking
+            board[row][col] = 0;
+        }
+    }
+
+}
+
+int main(){
+    int n = 6;
+    vector<vector<int>> board(n, vector<int>(n ,0));
+    int col = 0;
+    solve(board, col, n);
+    return 0;
+}
