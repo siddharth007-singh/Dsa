b270504c1037174db73a9010f18b1ef57a3bc73b
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-04-05 00:37:45 +0530

    Count_method

--- Best_Questions/Sort_Color.cpp
+++ Best_Questions/Sort_Color.cpp
@@ -13,10 +13,46 @@ int main(){
     // sort(arr.begin(), arr.end());
 
 
-    
+    //////////////////////////////////////////////////////////////Count Apporach
+
+    int zero, ones, tows = 0;
+
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]==0){
+            zero++;
+        }
+        else if(arr[i]==1){
+            ones++;
+        }
+        else{
+            tows++;
+        }
+    }
+
+    //Now Put intoteh aarray
+    int i=0;
+    while(zero--){
+        arr[i] = 0;
+    }
+    while(ones--){
+        arr[i] = 1;
+    }
+    while(tows--){
+        arr[i] = 2;
+    }
 
     for(int i=0; i<arr.size(); i++){
         cout<<arr[i];
     }
+
+
+
+    ///////////////////////////////////////////////////////////3Pointer apporach
+
+
+
+    // for(int i=0; i<arr.size(); i++){
+    //     cout<<arr[i];
+    // }
     return 0;
 }
