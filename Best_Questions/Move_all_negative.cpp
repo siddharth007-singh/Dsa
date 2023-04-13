99e43ce49a827cea20731812c68caa6022de8342
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-04-13 22:26:17 +0530

    Move_negative

--- Best_Questions/Move_all_negative.cpp
+++ Best_Questions/Move_all_negative.cpp
@@ -2,9 +2,32 @@
 #include<vector>
 #include<algorithm>
 
+
+//////Dutch National Algorithm  (2Pointer approch)
+
+
 using namespace std;
 
 int main(){
-    vector<int>arr{};
+    vector<int>arr{1, 2, -3, 4,-5, 6};
+
+    int l=0, h=arr.size()-1;
+
+    while(l<=h){
+        if(arr[l]<0){
+            l++;
+        }
+        else if(arr[h]>0){
+            h--;
+        }
+        else{
+            swap(arr[l], arr[h]);
+        }
+    }
+
+    for(int i=0; i<arr.size(); i++){
+        cout<<arr[i]<<" ";
+    }
+
     return 0;
 }
