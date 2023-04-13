da9fa56ad088132928166ae2d27698bc386bcc32
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-04-13 18:30:11 +0530

    New_Commit

--- Best_Questions/Sort_Color.cpp
+++ Best_Questions/Sort_Color.cpp
@@ -15,44 +15,64 @@ int main(){
 
     //////////////////////////////////////////////////////////////Count Apporach
 
-    int zero, ones, tows = 0;
+    // int zero, ones, tows = 0;
 
-    for(int i=0;i<arr.size();i++){
-        if(arr[i]==0){
-            zero++;
-        }
-        else if(arr[i]==1){
-            ones++;
-        }
-        else{
-            tows++;
-        }
-    }
+    // for(int i=0;i<arr.size();i++){
+    //     if(arr[i]==0){
+    //         zero++;
+    //     }
+    //     else if(arr[i]==1){
+    //         ones++;
+    //     }
+    //     else{
+    //         tows++;
+    //     }
+    // }
 
-    //Now Put intoteh aarray
-    int i=0;
-    while(zero--){
-        arr[i] = 0;
-    }
-    while(ones--){
-        arr[i] = 1;
-    }
-    while(tows--){
-        arr[i] = 2;
-    }
+    // //Now Put intoteh aarray
+    // int i=0;
+    // while(zero--){
+    //     arr[i] = 0;
+    // }
+    // while(ones--){
+    //     arr[i] = 1;
+    // }
+    // while(tows--){
+    //     arr[i] = 2;
+    // }
 
-    for(int i=0; i<arr.size(); i++){
-        cout<<arr[i];
-    }
-
-
-
-    ///////////////////////////////////////////////////////////3Pointer apporach
-
+    // for(int i=0; i<arr.size(); i++){
+    //     cout<<arr[i];
+    // }
 
 
     // for(int i=0; i<arr.size(); i++){
     //     cout<<arr[i];
     // }
+
+
+    ///////////////////////////////////////////////////////////3Pointer apporach
+
+    int l=0, m=0, h=arr.size()-1;
+
+    while(m<=h){
+        if(arr[m]==0){
+            swap(arr[l], arr[m]);
+            l++, m++;
+        }
+
+        else if(arr[m]==1){
+            m++;
+        }
+        else{
+            swap(arr[m],arr[h]);
+            h--;
+        }
+    }
+
+    for(int i=0; i<arr.size(); i++){
+        cout<<arr[i]<<" ";
+    }
+
     return 0;
 }
