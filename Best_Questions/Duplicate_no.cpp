fe9c998776b10253156375ecbfd67211b76a0f72
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-04-14 21:08:14 +0530

    Duplicate_ele

---
+++ Best_Questions/Duplicate_no.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+
+using namespace std;
+
+int findDuplicate(vector<int> &arr) 
+{ 
+    int ans = 0;
+    for(int i=0; i<arr.size(); i++){
+        ans = ans^arr[i];
+    }
+    
+    for(int i=1; i<arr.size(); i++){
+        ans = ans^i;
+    }
+    return ans;
+}
+
+int main(){
+    vector<int>arr{4, 2, 1, 3, 1};
+    cout<<findDuplicate(arr);
+    return 0;
+}
+
