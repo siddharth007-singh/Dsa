7d2513e191a7aea3af18d3bc6feef6e397c2daad
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-07-26 09:07:23 -0700

    New Commnt

--- Best_Questions/Find_Duplicate_no.cpp
+++ Best_Questions/Find_Duplicate_no.cpp
@@ -1,27 +1,50 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
-
+#include <bits/stdc++.h>
 
 using namespace std;
 
-int findDuplicate(vector<int> &arr) 
-{ 
-    int ans = 0;
-    for(int i=0; i<arr.size(); i++){
-        ans = ans^arr[i];
-    }
-    
-    for(int i=1; i<arr.size(); i++){
-        ans = ans^i;
-    }
-    return ans;
-}
+///// swapping the postion of the element with its respective index number;
+
+// int findDuplicate(vector<int> &arr){
+//     while(arr[0]!=arr[arr[0]]){
+//         swap(arr[0], arr[arr[0]]);
+//     }
+//     return arr[0];
+// }
+
+
+////without modifiying the array by using XOR techique;
+
+// int findDuplicate(vector<int> &arr){
+//     int ans =0;
+
+//     for(int i=0; i<arr.size(); i++){
+//         ans = ans^arr[i];
+//     }
+
+//     for(int i=1; i<arr.size(); i++){
+//         ans = ans^i;
+//     }
+
+//     return ans;
+// }
 
 int main(){
-    vector<int>arr{4, 2, 1, 3, 1};
 
-    cout<<findDuplicate(arr);
-    return 0;
+    //with modifing
+    vector<int> arr = {2,3,1,2,3};
+    // int n = sizeof(arr) / sizeof(arr[0]);
+
+    // sort(arr.begin(), arr.end());
+
+    // for(int i=0; i<n; i++){
+    //     if(arr[i]==arr[i+1]){
+    //         cout<<arr[i];
+    //     }
+    // }
+    // return -1;
+
+    int ans = findDuplicate(arr);
+    cout<<ans;
 }
-
--- Best_Questions/Frist_repeted_ele.cpp
+++ Best_Questions/Frist_repeted_ele.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
 #include <vector>
 #include<algorithm>
+#include <functional>
 
 using namespace std;
 
+// //0(n) Approch || best approch
+//Store the count of every element and then check frist element which in repeated and return that element
+//Store the count of the element by using hashing(C++ STL);
+int FristRepeat(vector<int> &arr){
+    unordered_map<int, int>hash;  
+    for(int i=0; i<arr.size(); i++){
+        hash[arr[i]]++;
+    }
+
+    for(int i=0; i<arr.size(); i++){
+        if(hash[arr[i]]>1){
+            return arr[i]; 
+        }
+    }
+    return -1;
+}
+
+
+//BruteFull Approch
+// int FristRepeat(vector<int> &arr){
+//     for(int i=0; i<arr.size(); i++){
+//         bool isRep = false;
+//         for(int j=i+1; j<arr.size(); j++){
+//             if(arr[i]==arr[j]){
+//                 isRep = true;
+//                 return arr[i];
+//             }
+//         }
+//     }
+//     return -1;
+// }
+
 
 int main(){
-    cout<<"Hellow World";
+    vector<int>arr{1,5,3,4,3,5,6}; 
+    int ans = FristRepeat(arr);
+    cout<<ans;
+    return 0;
 }
