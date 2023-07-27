40d072e06161a28b541fbdf95e103d6ad8f213b5
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-07-27 10:16:04 -0400

    new Commuit

--- Best_Questions/Multiple_duplicate.cpp
+++ Best_Questions/Multiple_duplicate.cpp
@@ -4,7 +4,24 @@
 
 using namespace std;
 
+int MultiDuplicate(vector<int> &arr, int n){
+    for(int i=0; i<arr.size(); i++){
+        int idx = arr[i]%arr.size();
+        arr[idx]+=arr.size();
+    }
+
+    for(int i=0; i<arr.size(); i++){
+        if((arr[i]/arr.size())>=2){
+            cout<<arr[i];
+        }
+    }
+        
+    // if(arr.size()==0) return {-1};
+}
+
 int main(){
-    cout<<"Hello World";
+    vector<int> arr{2,3,1,2,3};
+    int Mult = MultiDuplicate(arr);
+    cout<<Mult;
     return 0;
 }
