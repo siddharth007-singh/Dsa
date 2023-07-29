044e1c551745072248cc2df77672043fc55fecaa
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-07-29 03:48:36 -0400

    NEw Change

--- Basics_Question/SumofSeries.cpp
+++ Basics_Question/SumofSeries.cpp
@@ -4,16 +4,26 @@
 
 using namespace std;
 
-int SumofSeries(int n){
-    if(n==1)
-        return 1;
+//Through Recursion Method
 
-    int sum = n+SumofSeries(n-1);
+// int SumofSeries(int n){
+//     if(n==1)
+//         return 1;
+
+//     int sum = n+SumofSeries(n-1);
+//     return sum;
+// }
+
+
+//Throught Mathematics Induction formula
+int SumofSeries(int n){
+    int sum;
+    sum = n*(n+1)/2;
     return sum;
 }
 
 int main(){
-    int no = 1;
+    int no = 5;
     int ans= SumofSeries(no);
     cout<<ans;
     return 0;
