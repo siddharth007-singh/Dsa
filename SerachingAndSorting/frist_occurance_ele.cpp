0f13867fce6060e5bb3095e404a3c63ea39d74c7
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-07-31 20:50:22 +0530

    New comiit

--- SerachingAndSorting/frist_occurance_ele.cpp
+++ SerachingAndSorting/frist_occurance_ele.cpp
@@ -10,7 +10,35 @@
 
 using namespace std;
 
+int BinarySerach(vector<int> &arr, int k){
+    int s=0, e=arr.size()-1;
+
+    int mid = (s+e)/2;
+    int ans =-1;
+    while(s<=e){
+        if(arr[mid]==k){
+            //Stote the ans;
+            ans = mid;
+            //search the left section
+            e = mid-1;
+        }
+        else if(k<arr[mid]){
+            e = mid-1;
+        }
+        else{
+            s = mid+1;
+        }
+
+        mid = (s+e)/2;
+    }
+    return ans;
+}
+
 int main(){
     vector<int> arr{1,3,4,4,4,4,4,6,7,9};
-    
+    int k =4;
+    int ans = BinarySerach(arr, k);
+    cout<<ans;
+    return 0;
+
 }
