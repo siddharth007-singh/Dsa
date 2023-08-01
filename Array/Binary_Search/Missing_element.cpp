6d81cbd32f4740832c600b6730fd3db60203c608
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-08-01 10:16:28 +0530

    NEW COMMIT

---
+++ Array/Binary_Search/Missing_element.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int missing_ele(vector<int> &arr){
+    int s=0, e=arr.size();
+    int mid = (s+e)/2;
+
+    while(s<=e){
+        
+    }
+}
+
+int main(){
+    vector<int> arr{1,2,3,4,6,7,8};
+    int ans = missing_ele(arr);
+    cout<<ans;
+    return 0;
+}
+
+
+1:40:43
--- Array/Binary_Search/Peak_mountain.cpp
+++ Array/Binary_Search/Peak_mountain.cpp
@@ -4,18 +4,18 @@
 
 using namespace std;
 
-int missing_ele(vector<int> &arr){
+int Peak_Mountain(vector<int> &arr){
     int s=0, e=arr.size();
     int mid = (s+e)/2;
 
     while(s<=e){
-
+        
     }
 }
 
 int main(){
     vector<int> arr{1,2,3,4,6,7,8};
-    int ans = missing_ele(arr);
+    int ans = Peak_Mountain(arr);
     cout<<ans;
     return 0;
 }
