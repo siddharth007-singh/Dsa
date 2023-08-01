b74d5c751c872f821f6cac1bf89a3f8fb6493795
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-08-01 10:50:57 +0530

    peak mountain array

--- Binary_Search/Peak_mountain.cpp
+++ Binary_Search/Peak_mountain.cpp
@@ -5,20 +5,25 @@
 using namespace std;
 
 int Peak_Mountain(vector<int> &arr){
-    int s=0, e=arr.size();
-    int mid = (s+e)/2;
+    int s=0, e=arr.size()-1;
+    int mid = s+(e-s)/2;
 
-    while(s<=e){
-        
+    while(s<e){
+        if(arr[mid]<arr[mid]+1){
+            s = mid+1;
+        }
+        else{
+            e = mid;
+        }
+
+        mid = s+(e-s)/2;
     }
+    return s;
 }
 
 int main(){
-    vector<int> arr{1,2,3,4,6,7,8};
+    vector<int> arr{0,10,5,2};
     int ans = Peak_Mountain(arr);
     cout<<ans;
     return 0;
 }
-
-
-1:40:43
