d4dd04e6353c94cd96aae01668ac3b78e10b5390
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-08-02 20:32:27 +0530

    Sqaure Root

--- Binary_Search/Square_root.cpp
+++ Binary_Search/Square_root.cpp
@@ -4,13 +4,31 @@
 
 using namespace std;
 
-int Sqaure_root(int num){
-    
+int Sqaure_root(int n){
+    int s=0, e=n;
+    int ans;
+    int mid = (s+e)/2;
+
+    while(s<=e){
+        if(mid*mid==n){
+            return mid;
+        }
+        else if(mid*mid>n){
+            e = mid-1;
+        }
+        else{
+            //stotre the ans and do the search
+            ans = mid;
+            s = mid+1;
+        }
+        mid = (s+e)/2;
+    }
+    return ans;
 }
 
 int main(){
-    int num = 5;
-    int ans = Sqaure_root(num);
+    int key = 17;
+    int ans = Sqaure_root(key);
     cout<<ans;
     return 0;
 }
