27bf213d00f222805183461044e6ca627798f8bf
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-08-02 20:06:25 +0530

    New Commit

---
+++ Binary_Search/Square_root.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int missing_ele(vector<int> &arr){
+    
+}
+
+int main(){
+    vector<int> arr{1,2,3,4,6,7,8};
+    int ans = Sqaure_root(arr);
+    cout<<ans;
+    return 0;
+}
+
+
+1:40:43
