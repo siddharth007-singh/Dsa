a4831c9bcc76854c0267e8ffbe3b342ae775f874
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-08-03 20:51:43 +0530

    New Commit

--- Binary_Search/Search_Nearly_sorted_array.cpp
+++ Binary_Search/Search_Nearly_sorted_array.cpp
@@ -4,12 +4,34 @@
 
 using namespace std;
 
-int Peak_Mountain(vector<int> &arr){
-    
+int UsingBinarySearch(vector<int> &arr, int k){
+    int s=0, e=arr.size()-1;
+    while(s<=e){
+        int mid = s+(e-s)/2;
+        if(arr[mid]==k){
+            return mid;
+        }
+        else if(mid-1>=s && arr[mid-1]==k){
+            return mid-1;
+        }
+        else if(mid+1<e && arr[mid+1]==k){
+            return mid+1;
+        }
+
+        if(k>arr[mid]){
+            s = mid+2;
+        }
+        else{
+            e = mid-2;
+        }
+    }
+    return -1;
 }
 
 int main(){
     vector<int> arr{10,3,40,20,50,80,70};
-    
+    int target = 80;
+    int ans = UsingBinarySearch(arr, target);
+    cout<<ans;
     return 0;
 }
