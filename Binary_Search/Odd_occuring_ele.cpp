3ff79333dde8de812198ba7d3269e77b71cc9d1e
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-08-06 20:33:39 +0530

    new Commit

--- Binary_Search/Odd_occuring_ele.cpp
+++ Binary_Search/Odd_occuring_ele.cpp
@@ -1,11 +1,19 @@
+// Among 3 main type of binary search question "Observation in Index"
+
+
 #include<iostream>
 #include<vector>
 #include<algorithm>
 
 using namespace std;
 
+int OddOccurance(vector<int> &arr){
+    
+}
+
 int main(){
     vector<int> arr{1,1,2,2,3,3,4,4,3,600,600,4,4};
-    
+    int ans = OddOccurance(arr);
+    cout<<ans;
     return 0;
 }
