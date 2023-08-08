03cf3db665fe27c5d8f85ae06a05fe2cfb001e08
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-08-08 21:18:03 +0530

    New commit

---
+++ Binary_Search/search_in_sorted_rotated.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int SortedandRotated(vector<int> &arr){
+    // this is another program of finding the element in sorted and rotated array
+}
+
+int main(){
+    vector<int> arr{};
+    int ans = SortedandRotated(arr);
+    cout<<ans;
+    return 0;
+}
