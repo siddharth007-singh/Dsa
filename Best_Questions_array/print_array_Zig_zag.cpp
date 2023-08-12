c3a176ef45ad047a613b0947049acde6b380b0d8
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-08-12 18:29:44 +0530

    new commit

--- Best_Questions_array/print_array_Zig_zag.cpp
+++ Best_Questions_array/print_array_Zig_zag.cpp
@@ -6,10 +6,15 @@ using namespace std;
 
 void SortedandRotated(vector<int> &arr){
 
-    for(int i=0; i<arr,szie(); i++){
-        cout<<arr[i];
-        sidhu lol hai
-        gjglkjfhjffjhfhj
+    for(int i=0; i<arr,szie()-2; i++){
+        if(i%2==0){
+            if(arr[i]>arr[i+1]){
+                swap(arr[i], arr[i+1]);
+            }
+        }
+        else if(arr[i]<arr[i+1]){
+            swap(arr[i], arr[i+1]);
+        }
     }    
 }
 
