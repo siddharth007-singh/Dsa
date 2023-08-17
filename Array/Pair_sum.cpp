40ab540518ee80569dfe132e2399e1155c757af1
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-08-17 10:11:01 +0530

    new commit

--- Array/Pair_sum.cpp
+++ Array/Pair_sum.cpp
@@ -3,21 +3,39 @@
 
 using namespace std;
 
+void PairSum(vector<int> &arr, int sum){
+    int s=0;
+    int e = arr.size();
+
+    while(s<=e){
+        if(arr[s]+arr[e]==sum){
+            cout<<s;
+        }
+        else if(arr[s]+arr[e]>sum){
+            e--;
+        }
+        else{
+            s++;
+        }
+    }
+}
+
 int main(){
     vector<int> arr{10,20,30,60,40};
     int sum = 80;
 
-    for(int i=0;i<arr.size();i++){
-        int ele = arr[i];
+    // for(int i=0;i<arr.size();i++){
+    //     int ele = arr[i];
 
-        for(int j=i+1;j<arr.size();j++){
-            // cout<<"("<<arr[i]<<" "<<arr[j]<<")"<<endl;
+    //     for(int j=i+1;j<arr.size();j++){
+    //         // cout<<"("<<arr[i]<<" "<<arr[j]<<")"<<endl;
 
-            if(ele+arr[j]==sum){
-                cout<<"("<<ele<<","<<arr[j]<<")"<<endl;
-            }
-        }
-    }
-    
+    //         if(ele+arr[j]==sum){
+    //             cout<<"("<<ele<<","<<arr[j]<<")"<<endl;
+    //         }
+    //     }
+    // }
+
+    PairSum(arr, sum);
     return 0;
 }
