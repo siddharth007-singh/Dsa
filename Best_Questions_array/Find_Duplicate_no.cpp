86a599d86f85ae983f120d49343e68cf342ec921
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-08-19 23:57:56 +0530

    New Commit

--- Best_Questions_array/Find_Duplicate_no.cpp
+++ Best_Questions_array/Find_Duplicate_no.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 ///// swapping the postion of the element with its respective index number;
 
-// int findDuplicate(vector<int> &arr){
-//     while(arr[0]!=arr[arr[0]]){
-//         swap(arr[0], arr[arr[0]]);
-//     }
-//     return arr[0];
-// }
+int findDuplicate(vector<int> &arr){
+    while(arr[0]!=arr[arr[0]]){
+        swap(arr[0], arr[arr[0]]);
+    }
+    return arr[0];
+}
 
 
 ////without modifiying the array by using XOR techique;
@@ -34,17 +34,17 @@ int main(){
 
     //with modifing
     vector<int> arr = {2,3,1,2,3};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    // int n = sizeof(arr) / sizeof(arr[0]);
 
-    sort(arr.begin(), arr.end());
+    // sort(arr.begin(), arr.end());
 
-    for(int i=0; i<n; i++){
-        if(arr[i]==arr[i+1]){
-            cout<<arr[i];
-        }
-    }
-    return -1;
+    // for(int i=0; i<n; i++){
+    //     if(arr[i]==arr[i+1]){
+    //         cout<<arr[i];
+    //     }
+    // }
+    // return -1;
 
-    // int ans = findDuplicate(arr);
-    // cout<<ans;
+    int ans = findDuplicate(arr);
+    cout<<ans;
 }
