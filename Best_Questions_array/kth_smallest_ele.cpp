bd85d22f3a9ffba486d170b449ca8dadb5d814a3
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-09-13 18:02:52 +0530

    new Commit

---
+++ Best_Questions_array/kth_smallest_ele.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+int KthSamll(vector<int> &arr, int k){
+    sort(arr.begin(), arr.end());
+    return arr[k-1];
+}
+
+int main(){
+    vector<int> arr{7, 10, 4, 3, 20, 15};
+    int k = 3;
+    int ans = KthSamll(arr, k);
+    cout<<ans;
+    return 0;
+}
