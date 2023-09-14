d7824d1e2c9647132d85bab72dd96bd8202b9444
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-09-14 12:43:42 +0530

    new commit

--- Best_Questions_array/Rotate_array_by_k.cpp
+++ Best_Questions_array/Rotate_array_by_k.cpp
@@ -8,19 +8,27 @@ void RotateEle(vector<int> &arr, int k){
     // for(int i=0; i<arr.size();i++){
     //     arr[(i+k)%arr.size()]==arr[i];
     // }
+    // k%=arr.size();
 
     reverse(arr.begin(), arr.end());
-    reverse(arr.begin(), arr.end()-k);
-    reverse(arr.end()-k, arr.end());
+    reverse(arr.begin(), arr.end()+k);
+    reverse(arr.end()+k, arr.end());
 }
 
+// void printArray(vector<int> &arr){
+//     for(int i=0; i<arr.size(); i++){
+//         cout<<arr[i]<<" ";
+//     }
+// }
+
 int main(){
-    vector<int> arr{1, 2, 3, 4, 5, 6, 7};
-    int k = 2;
+    vector<int> arr{1, 2, 3, 4, 5, 6};
+    int k = 3;
     RotateEle(arr, k);
 
     for(int x:arr){
         cout<<x<<" ";
     }
+
     return 0;
 }
