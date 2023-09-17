d3d31764df4f968b727602adedd797a8be1773c8
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-09-18 00:15:43 +0530

    new Commit

---
+++ Basics_Question/Sum_eve_odd.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int reverse(int n)
+{
+    int rev = 0;
+    while (n != 0) {
+        rev = (rev * 10) + (n % 10);
+        n /= 10;
+    }
+    return rev;
+}
+
+void EvenSum(int n){
+    n = reverse(n);
+    int sumOdd = 0, sumEven = 0, c = 1;
+ 
+    while (n != 0) {
+        if (c % 2 == 0)
+            sumEven += n % 10;
+        else
+            sumOdd += n % 10;
+        n /= 10;
+        c++;
+    }
+ 
+    cout<<sumEven<<" "<<sumOdd;
+}
+
+int main() {
+	// Write your code here
+	int n;
+	cin>>n;
+	cout<<EvenSum(n);
+	return 0;
+}
