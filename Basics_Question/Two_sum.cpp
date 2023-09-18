059b5aae1129b2485c8e047c65171c6548e10967
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-09-18 23:36:33 +0530

    new Comit

--- Basics_Question/Two_sum.cpp
+++ Basics_Question/Two_sum.cpp
@@ -1,23 +1,32 @@
 // So, for this problem we can solve in 3 ways 1.BrutFull approch (O(n2))  2.Best approh using Hashing (O(nLongN))  3. optimal approch using two pointer (O(N));
 #include<iostream>
 #include<vector>
+#include<unordered_map>
+#include<algorithm>
 
 using namespace std;
 
 //BruteFull approch
+// void CheckSum(vector<int> &arr, int k){
+//     for(int i=0; i<arr.size(); i++){
+//         for(int j=i+1; j<arr.size(); j++){
+//             if(arr[i]+arr[j]==k){
+//                 cout<<arr[i]<<" "<<arr[j];
+//                 break;
+//             }
+//         }
+//     }
+// }
+
+// //Best approch
 void CheckSum(vector<int> &arr, k){
-    
+
 }
 
-//Best approch
-void CheckSum(vector<int> &arr, k){
+// //Optimal approch
+// void CheckSum(vector<int> &arr, k){
 
-}
-
-//Optimal approch
-void CheckSum(vector<int> &arr, k){
-
-}
+// }
 
 int main(){
     vector<int> arr{3,2,4};
