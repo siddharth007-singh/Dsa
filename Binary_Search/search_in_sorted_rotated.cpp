2989743bf16cfd9a9835e69cd48db67063bb7ca2
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-09-20 03:45:50 +0530

    new Commit

--- Binary_Search/search_in_sorted_rotated.cpp
+++ Binary_Search/search_in_sorted_rotated.cpp
@@ -35,11 +35,39 @@ using namespace std;
 // }
 
 // //For Dupicate eleemnts 
-int SortedandRotated(vector<int> &arr){
+int SortedandRotated(vector<int> &arr, int k){
     int s=0, e=arr.size()-1;
     int mid;
 
-    
+    while(s<=e){
+        mid = (s+e)/2;
+
+        if(arr[mid]==k) return mid;
+
+        if(arr[s]==arr[mid] && arr[mid]==arr[e]){
+            s++;
+            e--;
+            continue;
+        }
+
+        if(arr[s]<=arr[mid]){
+            if(arr[s]<=k && k<=arr[mid]){
+                e = mid-1;
+            }
+            else{
+                s = mid+1;
+            }
+        }
+        else{
+            if(arr[mid]<=k && k<=arr[e]){
+                s = mid+1;
+            }
+            else{
+                e = mid-1;
+            }
+        }
+    }
+    return -1;
 }
 
 int main(){
