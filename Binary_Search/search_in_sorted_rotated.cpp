e9acc27ab479cfcd27541a61b65bea0d8608a560
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-09-20 03:23:02 +0530

    New Commit

--- Binary_Search/search_in_sorted_rotated.cpp
+++ Binary_Search/search_in_sorted_rotated.cpp
@@ -4,14 +4,49 @@
 
 using namespace std;
 
+//for Nonduplicate element
+// int SortedandRotated(vector<int> &arr, int k){
+//     int s=0,e=arr.size()-1;
+//     int mid;
+    
+//     while(s<=e){
+//         mid = (s+e)/2;
+
+//         if(arr[mid]==k) return mid;
+
+//         if(arr[s]<=arr[mid]){
+//             if(arr[s]<=k && k<=arr[mid]){
+//                 e = mid-1;
+//             }
+//             else{
+//                 s = mid+1;
+//             }
+//         }
+//         else{
+//             if(arr[mid]<=k && k<=arr[e]){
+//                 s = mid+1;
+//             }
+//             else{
+//                 e= mid-1;
+//             }
+//         }
+//     }
+//     return -1;
+// }
+
+// //For Dupicate eleemnts 
 int SortedandRotated(vector<int> &arr){
-    // this is another program of finding the element in sorted and rotated array
+    int s=0, e=arr.size()-1;
+    int mid;
+
     
 }
 
 int main(){
-    vector<int> arr{};
-    int ans = SortedandRotated(arr);
+    // vector<int> arr{8, 9, 4, 5};
+    vector<int> arr{7,8,1,2,3,3,3,4,5,6};
+    int k = 3;
+    int ans = SortedandRotated(arr,k);
     cout<<ans;
     return 0;
 }
