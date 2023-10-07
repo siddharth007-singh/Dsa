90da6d4dd948846cff670dfa2012d0e6f98be396
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-10-07 11:17:45 +0530

    new Commit

--- Best_Question_String/Convert_lowe_upper.cpp
+++ Best_Question_String/Convert_lowe_upper.cpp
@@ -2,9 +2,32 @@
 
 #include<iostream>
 #include<algorithm>
+#include<string.h>
 
 using namespace std;
 
+void convertToUppercase(char arr[]){
+    int n = strlen(arr);
+
+    for(int i=0; i<n; i++){
+        arr[i] = arr[i]-'a'+'A';
+    }
+}
+
+void convertToLowercase(char arr[]){
+    int n = strlen(arr);
+
+    for(int i=0; i<n; i++){
+        arr[i] = arr[i]-'A'+'a';
+    }
+}
+
 int main(){
+    // char arr[100] = "testing";
+    // convertToUppercase(arr);
+
+    char arr[100] = "TESTING";
+    convertToLowercase(arr);
+    cout<<arr<<endl;
     return 0;
 }
