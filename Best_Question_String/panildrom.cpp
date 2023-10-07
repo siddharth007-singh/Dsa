61d239ad2361094e98cf60526f4b93ee8aae972e
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-10-07 10:38:28 +0530

    new commit

---
+++ Best_Question_String/panildrom.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+
+using namespace std;
+
+bool panildrom(char arr[]){
+    int i=0;
+    int n = strlen(arr);
+    int j = n-1;
+
+    while(i>j){
+        if(arr[i]!=arr[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+
+int main(){
+    char arr[100] = "BabbaB";
+    cin>>arr;
+    if(panildrom(arr)){
+        cout<<"yes";
+    }
+    else{
+        cout<<"no";
+    }
+
+    return 0;
+}
