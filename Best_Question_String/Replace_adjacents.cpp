2f18cb5b0578d3a4aa22410e9fce9bff57b8141c
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-10-08 15:26:03 +0530

    new Comiit

---
+++ Best_Question_String/Replace_adjacents.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ASUS on 10/8/2023.
+//
+
+#include "Replace_adjacents.h"
---
+++ Best_Question_String/Replace_adjacents.h
@@ -0,0 +1,16 @@
+//
+// Created by ASUS on 10/8/2023.
+//
+
+#ifndef DSA_REPLACE_ADJACENTS_H
+#define DSA_REPLACE_ADJACENTS_H
+
+
+
+class Replace_adjacents {
+
+};
+
+
+
+#endif //DSA_REPLACE_ADJACENTS_H
