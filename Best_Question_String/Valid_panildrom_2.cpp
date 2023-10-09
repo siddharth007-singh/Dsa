ba696b31490abe50815f6026e9b480a1331888d4
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-10-10 02:18:52 +0530

    new Comkit

---
+++ Best_Question_String/Valid_panildrom_2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+bool panildrom(char arr[]){
+    
+}
+
+int main(){
+    char arr[100] = "abcc";
+    cin>>arr;
+    if(panildrom(arr)){
+        cout<<"yes";
+    }
+    else{
+        cout<<"no";
+    }
+
+    return 0;
+}
