85be8121b5a3030dcd2f95bdb9b66b5bdeef6636
siddharth007-singh <siddharth.code.cws@gmail.com>
2023-10-11 00:06:40 +0530

    new Commit

--- Best_Question_String/Valid_panildrom_2.cpp
+++ Best_Question_String/Valid_panildrom_2.cpp
@@ -3,12 +3,29 @@
 
 using namespace std;
 
+bool checkPanildrom(string s, int i, int j){
+    while(i<=j){
+        if(s[i]!=s[j]) return false;
+        i++;
+        j--;
+    }
+
+    return true;
+}
+
 bool panildrom(char arr[]){
     int n = arr.length();
     int i=0, j=n-1;
 
     while(i<=j){
-        
+        if(arr[i]!=arr[j]){
+            //Do remove i or j  Something
+            return checkPanildrom(arr, i+1, j) || checkPanildrom(arr, i, j-1);
+        }
+        else{
+            i++;
+            j--;
+        }
     }
 }
 
