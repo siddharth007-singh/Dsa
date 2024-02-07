e3f6690cf23f5f389c4f66ee9a69eb387c788b53
siddharth007-singh <siddharth.code.cws@gmail.com>
2024-02-07 21:10:03 +0530

    New Commit

--- Best_Question_String/Remove_Adjacent_Duplicates_String_II.cpp
+++ Best_Question_String/Remove_Adjacent_Duplicates_String_II.cpp
@@ -1,10 +1,39 @@
 #include<iostream>
 #include<string.h>
+#include <algorithm>
+#include<stack>
 
 using namespace std;
 
 string removeAdjacentString(string s, int k){
+    string ans = "";
+   int n = s.size();
+   if(n<k) return s;
 
+
+   stack<pair<char, int>> stack;
+
+   for(int i=0; i<n; i++){
+    if(stack.empty() || stack.top().first!=s[i]){
+        stack.push({s[i], 1});
+    }
+    else{
+        auto prev = stack.top();
+        stack.pop();
+        stack.push({s[i], prev.second+1});
+    }
+    if(stack.top().second==k) stack.pop();
+   } 
+
+    while (!stack.empty()){
+        auto cur = stack.top();
+        stack.pop();
+        while(cur.second--){
+            ans.push_back(cur.first);
+        }
+    }
+    reverse(ans.begin, ans.end());
+    return ans;
 }
 
 
