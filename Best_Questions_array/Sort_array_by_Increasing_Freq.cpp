06de211b1c1618f097ae45ea069143759bafd1bc
siddharth007-singh <siddharth.code.cws@gmail.com>
2024-02-09 20:03:03 +0530

    This Lembda VVIP

--- Best_Questions_array/Sort_array_by_Increasing_Freq.cpp
+++ Best_Questions_array/Sort_array_by_Increasing_Freq.cpp
@@ -5,18 +5,23 @@
 
 using namespace std;
 
-void findRepeatingElements(vector<int> &arr){
+vector<int> findRepeatingElements(vector<int> &arr){
     unordered_map<int, int>hash;
     for(int i=0;i<arr.size(); i++){
         hash[arr[i]]++;
     }
 
-    for (auto x : hash)
-        cout << x.first << "->" << x.second << endl;
+    sort(arr.begin(), arr.end(), [&](int a, int b){
+        if(hash[a]==hash[b])
+            return a>b;
+        else return hash[a]<hash[b];
+    });
+
+    return arr;
 }
 
 int main(){
-    vector<int> arr{1,1,2,3,4,4,5,2};   
+    vector<int> arr{1,1,2,3,4,4,5,2};
     findRepeatingElements(arr);
     return 0;
 }
