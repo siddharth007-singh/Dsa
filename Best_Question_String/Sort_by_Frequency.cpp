bd601a0bcfc038456e2b2a0cbca14a9e2c2bd12f
siddharth007-singh <siddharth.code.cws@gmail.com>
2024-02-11 19:48:28 +0530

    NEw Commit VVIP

--- Best_Question_String/Sort_by_Frequency.cpp
+++ Best_Question_String/Sort_by_Frequency.cpp
@@ -5,8 +5,32 @@
 
 using namespace std;
 
+bool cond(pair<char, int> &a, pair<char, int> &b){
+    return a.second>b.second;
+}
+
 string SortFrequecy(string s){
-    
+    string ans;
+    unordered_map<char, int> hash;
+    for(int i=0; i<s.length(); i++){
+        hash[s[i]]++;
+    }
+
+    vector<pair<char, int>>v;
+    for(auto x:hash){
+        v.push_back({x.first, x.second});
+    }
+
+    sort(v.begin(), v.end(), cond);
+
+    //now after sorting push all the elements in the string ans;
+    for(auto x:v){
+        for(int i=0; i<x.second; i++){
+            ans+=x.first;
+        }
+    }
+
+    return ans;
 }
 
 int main(){
